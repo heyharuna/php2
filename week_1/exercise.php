f33611ca1f51777a71312a4924d75224925843d3
heyharuna <heyharuna0221@gmail.com>
2017-09-11 16:00:22 -0700

    create two directory

---
+++ week_1/exercise.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>About Page</title>
+        <link rel="stylesheet" href="css/foundation.css" />
+        <link rel="stylesheet" href="style.css" />
+        <script src="js/vendor/modernizr.js"></script>
+    </head>
+    <body>
+      <?php
+        class Person {
+            private $name = "";
+          public function _constract($name){
+            $this ->name = $name;
+          }
+          public function getName() {
+
+          }
+        }
+
+      ?>
+    </body>
+</html>
---
+++ week_2/index.php
@@ -0,0 +1,26 @@
+
+<?php
+  class SystemCar {
+    var $power = 0;
+
+    function turnOn() {
+      $this -> power = 1;
+    }
+    function turnOff() {
+
+    }
+
+  }
+
+  class Car extends SystemCar {
+    function car() {
+
+    }
+  }
+
+
+
+
+
+
+?>
